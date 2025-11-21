7a412cc666079521606e0ce824486c0b365ea0bc
Raul Rabadan Arroyo <raul@rabadan.dev>
2025-11-21 21:39:17 +0100

    xtask: Add installdeps/format/clippy/test/wasmtest/check commands

--- xtask/src/cli.rs
+++ xtask/src/cli.rs
@@ -1,4 +1,5 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
+use devx_cmd::cmd;
 use devx_pre_commit::locate_project_root;
 
 #[derive(Parser)]
@@ -11,6 +12,12 @@ impl Cli {
     pub fn eval(self) -> anyhow::Result<()> {
         match Cli::parse().command {
             Command::InstallPreCommitHook => run_install_pre_commit_hook(),
+            Command::InstallDependencies => run_install_dependencies(),
+            Command::Format(format_opts) => run_format(format_opts),
+            Command::Clippy => run_clippy(),
+            Command::RegularTest => run_regular_tests(),
+            Command::WasmTest(wasm_test_opts) => run_wasm_tests(wasm_test_opts),
+            Command::Checks => run_checks(),
         }
     }
 }
@@ -18,9 +25,147 @@ impl Cli {
 #[derive(Subcommand)]
 enum Command {
     InstallPreCommitHook,
+    InstallDependencies,
+    Format(FormatOpts),
+    Clippy,
+    RegularTest,
+    WasmTest(WasmTestOpts),
+    Checks,
+}
+
+#[derive(Args)]
+struct InstallDependenciesOpts {
+    #[command(subcommand)]
+    mode: Option<InstallDependenciesMode>,
+}
+
+#[derive(Copy, Clone, Subcommand)]
+enum InstallDependenciesMode {
+    /// Install dependencies from pre-compiled binaries
+    CI,
+    /// Install dependencies through `cargo install`
+    Development,
+}
+
+#[derive(Args)]
+struct FormatOpts {
+    /// Whether to check only or not
+    #[arg(short, long)]
+    check_only: bool,
+}
+
+#[derive(Args)]
+struct WasmTestOpts {
+    #[command(subcommand)]
+    mode: Option<WasmTestMode>,
+}
+
+#[derive(Copy, Clone, Subcommand)]
+enum WasmTestMode {
+    /// Test with headless Chrome and Firefox
+    HeadlessChromeAndFirefox,
+    /// Test with headless Chrome
+    HeadlessChrome,
+    /// Test with headless Firefox,
+    HeadlessFirefox,
+    /// Test with non-headless Chrome
+    Chrome,
+    /// Test with non-headless Firefox,
+    Firefox,
 }
 
 fn run_install_pre_commit_hook() -> anyhow::Result<()> {
     devx_pre_commit::install_self_as_hook(&locate_project_root()?)?;
+    Ok(())
+}
+
+fn run_install_dependencies() -> anyhow::Result<()> {
+    let root = locate_project_root()?;
+
+    cmd!("cargo", "install", "wasm-pack", "--version", "^0.13")
+        .current_dir(&root)
+        .log_cmd(log::Level::Trace)
+        .log_err(log::Level::Trace)
+        .run()?;
+
+    Ok(())
+}
+
+fn run_format(opts: FormatOpts) -> anyhow::Result<()> {
+    let root = locate_project_root()?;
+
+    if opts.check_only {
+        cmd!("cargo", "fmt", "--check")
+    } else {
+        cmd!("cargo", "fmt")
+    }
+    .current_dir(&root)
+    .log_cmd(log::Level::Trace)
+    .log_err(log::Level::Trace)
+    .run()?;
+
+    Ok(())
+}
+
+fn run_clippy() -> anyhow::Result<()> {
+    let root = locate_project_root()?;
+
+    cmd!("cargo", "clippy", "--all-targets")
+        .current_dir(&root)
+        .log_cmd(log::Level::Trace)
+        .log_err(log::Level::Trace)
+        .run()?;
+
+    Ok(())
+}
+
+fn run_regular_tests() -> anyhow::Result<()> {
+    let root = locate_project_root()?;
+
+    cmd!("cargo", "test")
+        .current_dir(&root)
+        .log_cmd(log::Level::Trace)
+        .log_err(log::Level::Trace)
+        .run()?;
+
+    Ok(())
+}
+
+fn run_wasm_tests(opts: WasmTestOpts) -> anyhow::Result<()> {
+    let root = locate_project_root()?;
+
+    match opts.mode.unwrap_or(WasmTestMode::HeadlessChromeAndFirefox) {
+        WasmTestMode::HeadlessChromeAndFirefox => {
+            cmd!("wasm-pack", "test", "--headless", "--chrome", "--firefox")
+        }
+        WasmTestMode::HeadlessChrome => {
+            cmd!("wasm-pack", "test", "--headless", "--chrome")
+        }
+        WasmTestMode::HeadlessFirefox => {
+            cmd!("wasm-pack", "test", "--headless", "--firefox")
+        }
+        WasmTestMode::Chrome => {
+            cmd!("wasm-pack", "test", "--chrome")
+        }
+        WasmTestMode::Firefox => {
+            cmd!("wasm-pack", "test", "--firefox")
+        }
+    }
+    .current_dir(&root)
+    .log_cmd(log::Level::Trace)
+    .log_err(log::Level::Trace)
+    .run()?;
+
+    Ok(())
+}
+
+pub fn run_checks() -> anyhow::Result<()> {
+    run_format(FormatOpts { check_only: true })?;
+    run_clippy()?;
+    run_regular_tests()?;
+    run_wasm_tests(WasmTestOpts {
+        mode: Some(WasmTestMode::HeadlessChromeAndFirefox),
+    })?;
+
     Ok(())
 }
