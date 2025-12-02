733528afa1f4c3a7206071a4613afaa76ec603bf
Raul Rabadan Arroyo <raul@rabadan.dev>
2025-12-02 11:15:46 +0100

    clippy: Supress warning of futures not being send

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 #![warn(clippy::nursery)]
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
+// We work with JsFutures, which are not send due to JavaScript being inherently
+// single-threaded. Therefore, we need to allow this.
+#![allow(clippy::future_not_send)]
 
 #[cfg(test)]
 pub mod test_utils;
