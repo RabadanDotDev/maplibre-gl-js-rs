070048fca44f78e0dce259eb078625cbb00b0b94
Raul Rabadan Arroyo <raul@rabadan.dev>
2025-12-06 19:17:13 +0100

    feat(interface): Add LngLatLike

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,7 +1,9 @@
 //! Rusty interface to the bindings of MapLibre GL JS
 
 mod lng_lat;
+mod lng_lat_like;
 
 pub use serde_wasm_bindgen::Error;
 
 pub use lng_lat::LngLat;
+pub use lng_lat_like::LngLatLike;
