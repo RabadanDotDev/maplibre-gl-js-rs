2f5f04fec5168b77655b1d67f89b908223af2516
Raul Rabadan Arroyo <raul@rabadan.dev>
2025-12-06 21:08:56 +0100

    example: Add Display a map with HTMLElement example

---
+++ examples/display-a-map-with-html-container/README.md
@@ -0,0 +1,4 @@
+# Display a map with HTMLElement
+
+Initialize a map with Yew and the unofficial MapLibre GL JS Rust bindings by passing an HTMLElement reference.
+
---
+++ examples/display-a-map-with-html-container/src/main.rs
@@ -0,0 +1,31 @@
+use maplibre_gl_js::interface::{HtmlElement, MapOptions};
+use yew::{Html, function_component, html, use_effect, use_node_ref};
+
+#[function_component(App)]
+fn app() -> Html {
+    let container_ref = use_node_ref();
+
+    {
+        let container_ref = container_ref.clone();
+        use_effect(move || {
+            let container = container_ref
+                .cast::<HtmlElement>()
+                .expect("Container should be a valid HtmlElement");
+            MapOptions::new(container)
+                .with_style("https://demotiles.maplibre.org/style.json")
+                .with_center([0., 0.])
+                .with_zoom(1.0)
+                .with_maplibre_logo()
+                .build()
+                .expect("Creating a map should work");
+        });
+    }
+
+    html! { <div ref={container_ref}></div> }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    console_error_panic_hook::set_once();
+    yew::Renderer::<App>::new().render();
+}
