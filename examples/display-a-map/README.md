4eb0476b58cf6729fa5dff947a0547e320f1460d
Raul Rabadan Arroyo <raul@rabadan.dev>
2025-12-06 20:59:21 +0100

    example: Add Display a map example

---
+++ examples/display-a-map/README.md
@@ -0,0 +1,4 @@
+# Display a map
+
+Initialize a map with Yew and the unofficial MapLibre GL JS Rust bindings.
+
---
+++ examples/display-a-map/src/main.rs
@@ -0,0 +1,22 @@
+use maplibre_gl_js::interface::MapOptions;
+use yew::{Html, function_component, html, use_effect};
+
+#[function_component(App)]
+fn app() -> Html {
+    use_effect(move || {
+        MapOptions::new("map")
+            .with_style("https://demotiles.maplibre.org/style.json")
+            .with_center([0., 0.])
+            .with_zoom(1.0)
+            .with_maplibre_logo()
+            .build()
+            .expect("Creating a map should work");
+    });
+    html! { <div id="map"></div> }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    console_error_panic_hook::set_once();
+    yew::Renderer::<App>::new().render();
+}
