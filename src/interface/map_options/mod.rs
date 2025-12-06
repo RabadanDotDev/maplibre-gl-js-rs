b2e36b8422506c4d9336f119da835ee89ed09b58
Raul Rabadan Arroyo <raul@rabadan.dev>
2025-12-06 19:47:35 +0100

    feat(interface): Add MapZoom struct for MapOptions field

--- src/interface/map_options/mod.rs
+++ src/interface/map_options/mod.rs
@@ -1,7 +1,9 @@
 //! Rusty interface to the bindings of `MapOptions` and additional type checks of MapLibre GL JS
 
 mod map_container;
+mod map_zoom;
 
 use super::Error;
 
 pub use map_container::MapContainer;
+pub use map_zoom::MapZoom;
