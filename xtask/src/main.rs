1a7b0127dff86cfc499d6891c08aa73bdba6ba56
Raul Rabadan Arroyo <raul@rabadan.dev>
2025-12-12 13:03:44 +0100

    xtask: Add book preprocessor to generate examples

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -3,7 +3,9 @@ use clap::Parser as _;
 use xtask::{Cli, run_if_precommit};
 
 fn main() -> anyhow::Result<()> {
-    simple_logger::SimpleLogger::new().init()?;
+    simple_logger::SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .init()?;
     if run_if_precommit()? {
         Ok(())
     } else {
