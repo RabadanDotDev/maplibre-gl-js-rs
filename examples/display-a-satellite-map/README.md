292c544d9907ca3f2d66396d4b37b783a97d137a
Raul Rabadan Arroyo <raul@rabadan.dev>
2025-12-23 12:59:30 +0100

    example: Add Display a satellite map example

---
+++ examples/display-a-satellite-map/README.md
@@ -0,0 +1,4 @@
+# Display a satellite map
+
+Display a satellite raster baselayer.
+
---
+++ examples/display-a-satellite-map/src/main.rs
@@ -0,0 +1,37 @@
+use maplibre_gl_js::interface::MapOptions;
+use yew::{Html, function_component, html, use_effect};
+
+#[function_component(App)]
+fn app() -> Html {
+    use_effect(move || {
+        MapOptions::new("map")
+            .with_center([137.9150899566626, 36.25956997955441])
+            .with_zoom(9.)
+            .with_style(serde_json::json!({
+                "version": 8,
+                "sources": {
+                    "satellite": {
+                        "type": "raster",
+                        "tiles": [
+                            "https://tiles.maps.eox.at/wmts/1.0.0/s2cloudless-2020_3857/default/g/{z}/{y}/{x}.jpg"
+                        ],
+                        "tileSize": 256
+                    }
+                },
+                "layers": [{
+                    "id": "satellite",
+                    "type": "raster",
+                    "source": "satellite"
+                }]
+            }))
+            .build()
+            .expect("Creating a map should work");
+    });
+    html! { <div id="map"></div> }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    console_error_panic_hook::set_once();
+    yew::Renderer::<App>::new().render();
+}
