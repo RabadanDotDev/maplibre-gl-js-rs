0693aa308915c3529967b705fd6f8e9436713a5d
Raul Rabadan Arroyo <raul@rabadan.dev>
2026-01-01 18:14:46 +0100

    refactor!(interface): Generate MapOptions fields through a declarative macro
    The pub specifier has also been removed. It is not necessary to expose
    it and it allows to add hidden fields without extra macro parameters.

--- src/interface/map_options/mod.rs
+++ src/interface/map_options/mod.rs
@@ -16,109 +16,154 @@ use crate::{
     interface::{LngLatLike, Map},
 };
 
-/// Options of a Map
-#[derive(Debug, PartialEq, Clone, Serialize)]
-pub struct MapOptions {
-    /// The container of the map
-    pub container: MapContainer,
-    /// URL MapLibre style.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub style: Option<MapStyleOption>,
-    /// Initial zoom level of the map. Defaults to the style or `0`
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub zoom: Option<MapZoom>,
-    /// Whether to show the MapLibre logo
-    #[serde(skip_serializing_if = "Option::is_none", rename = "maplibreLogo")]
-    pub maplibre_logo: Option<bool>,
-    /// Initial position of the map. Defaults to the style or the center
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub center: Option<LngLatLike>,
-    /// Whether the map is interactive or not. Defaults to `true`
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub interactive: Option<bool>,
-    // TODO: Support all options
+/// Generate `MapOptions` struct with builder functions
+macro_rules! declare_map_options {
+    (
+        $(
+            {
+                name: $name:ident,
+                serde_rename: $serde_rename:expr,
+                inner_type: $inner_type:ty,
+                type_docs: $type_docs:expr,
+                implement: $implement:ident,
+                implement_docs: $implement_docs:expr,
+            }
+        ),* $(,)?
+    ) => {
+        /// Options of a map
+        #[derive(Debug, PartialEq, Clone, Serialize)]
+        pub struct MapOptions {
+            /// The container of the map
+            container: MapContainer,
+            $(
+                #[doc = $type_docs]
+                #[serde(
+                    skip_serializing_if = "Option::is_none",
+                    rename = $serde_rename
+                )]
+                $name: Option<$inner_type>,
+            )*
+        }
+
+        impl MapOptions {
+            /// Create new `MapOptions` with the given container
+            pub fn new(container: impl Into<MapContainer>) -> Self {
+                Self {
+                    container: container.into(),
+                    $(
+                        $name: None,
+                    )*
+                }
+            }
+
+            /// Create a `Map` with the specified options
+            ///
+            /// # Errors
+            ///
+            /// Propagates errors generated on converting the given options into JS
+            pub fn build(self) -> Result<Map, Error> {
+                Map::new(self)
+            }
+
+            /// Try converting `MapOptions` into the equivalent bindings type
+            ///
+            /// # Errors
+            ///
+            /// Propagates `serde_wasm_bindgen` conversion errors
+            pub fn as_js_value(&self) -> Result<bindings::MapOptions, super::Error> {
+                Ok(bindings::MapOptions::from(self.serialize(
+                    &serde_wasm_bindgen::Serializer::json_compatible(),
+                )?))
+            }
+
+            $(
+                declare_map_options!(@impl $implement, $name, $inner_type, $implement_docs);
+            )*
+        }
+    };
+
+    (@impl nothing, $name:ident, $inner_type:ty, $implement_docs:expr) => {
+    };
+    (@impl set_type, $name:ident, $inner_type:ty, $implement_docs:expr) => {
+        paste::paste! {
+            #[doc = $implement_docs]
+            #[must_use]
+            pub fn [<with_ $name>](self, $name: impl Into<$inner_type>) -> Self {
+                Self {
+                    $name: Some($name.into()),
+                    ..self
+                }
+            }
+        }
+    };
+    (@impl set_true, $name:ident, $inner_type:ty, $implement_docs:expr) => {
+        paste::paste! {
+            #[doc = $implement_docs]
+            #[must_use]
+            pub fn [<with_ $name>](self) -> Self {
+                Self {
+                    $name: Some(true),
+                    ..self
+                }
+            }
+        }
+    };
+    (@impl set_false, $name:ident, $inner_type:ty, $implement_docs:expr) => {
+        paste::paste! {
+            #[doc = $implement_docs]
+            #[must_use]
+            pub fn [<without_ $name>](self) -> Self {
+                Self {
+                    $name: Some(false),
+                    ..self
+                }
+            }
+        }
+    };
 }
 
-impl MapOptions {
-    /// Create new `MapOptions` with the given container
-    pub fn new(container: impl Into<MapContainer>) -> Self {
-        Self {
-            container: container.into(),
-            style: None,
-            zoom: None,
-            maplibre_logo: None,
-            center: None,
-            interactive: None,
-        }
-    }
-
-    /// Create a `Map` with the specified options
-    ///
-    /// # Errors
-    ///
-    /// Propagates errors generated on converting the given options into JS
-    pub fn build(self) -> Result<Map, Error> {
-        Map::new(self)
-    }
-
-    /// Specify initial zoom level of the map
-    #[must_use]
-    pub fn with_zoom(self, zoom: impl Into<MapZoom>) -> Self {
-        Self {
-            zoom: Some(zoom.into()),
-            ..self
-        }
-    }
-
-    /// Specify the `Map`'s style
-    #[must_use]
-    pub fn with_style(self, style: impl Into<MapStyleOption>) -> Self {
-        Self {
-            style: Some(style.into()),
-            ..self
-        }
-    }
-
-    /// Specify if to show the MapLibre logo in the `Map`'
-    #[must_use]
-    pub fn with_maplibre_logo(self) -> Self {
-        Self {
-            maplibre_logo: Some(true),
-            ..self
-        }
-    }
-
-    /// Specify the starting position of the map
-    #[must_use]
-    pub fn with_center(self, center: impl Into<LngLatLike>) -> Self {
-        Self {
-            center: Some(center.into()),
-            ..self
-        }
-    }
-
-    /// Mark the map as not interactive
-    #[must_use]
-    pub fn without_interactivity(self) -> Self {
-        Self {
-            interactive: Some(false),
-            ..self
-        }
-    }
-
-    /// Try converting `MapOptions` into the equivalent bindings type
-    ///
-    /// # Errors
-    ///
-    /// Propagates `serde_wasm_bindgen` conversion errors
-    pub fn as_js_value(&self) -> Result<bindings::MapOptions, super::Error> {
-        Ok(bindings::MapOptions::from(self.serialize(
-            &serde_wasm_bindgen::Serializer::json_compatible(),
-        )?))
-    }
-
-    // TODO: Add all the methods
-}
+declare_map_options!(
+    {
+        name: center,
+        serde_rename: "center",
+        inner_type: LngLatLike,
+        type_docs: "Initial position of the map. Defaults to the style or to 0,0",
+        implement: set_type,
+        implement_docs: "Specify the starting position of the map",
+    },
+    {
+        name: interactivity,
+        serde_rename: "interactive",
+        inner_type: bool,
+        type_docs: "Whether the map is interactive or not. Defaults to `true`",
+        implement: set_false,
+        implement_docs: "Mark the map as not interactive",
+    },
+    {
+        name: maplibre_logo,
+        serde_rename: "maplibreLogo",
+        inner_type: bool,
+        type_docs: "Whether to show the MapLibre logo",
+        implement: set_true,
+        implement_docs: "Specify if to show the MapLibre logo in the `Map`",
+    },
+    {
+        name: style,
+        serde_rename: "style",
+        inner_type: MapStyleOption,
+        type_docs: "MapLibre style",
+        implement: set_type,
+        implement_docs: "Specify the `Map`'s style",
+    },
+    {
+        name: zoom,
+        serde_rename: "zoom",
+        inner_type: MapZoom,
+        type_docs: "Initial zoom level of the map. Defaults to the style or `0`",
+        implement: set_type,
+        implement_docs: "Specify initial zoom level of the map",
+    },
+);
 
 impl TryFrom<MapOptions> for bindings::MapOptions {
     type Error = super::Error;
