33710c61d115c514c20835d90677adb8adf8e96b
Raul Rabadan Arroyo <raul@rabadan.dev>
2026-01-04 14:18:11 +0100

    example: Add 'Add a raster tile source' example

---
+++ examples/add-a-raster-tile-source/README.md
@@ -0,0 +1,4 @@
+# Add a raster tile source
+
+Add a third-party raster source to the map.
+
---
+++ examples/add-a-raster-tile-source/src/main.rs
@@ -0,0 +1,41 @@
+use maplibre_gl_js::interface::MapOptions;
+use yew::{Html, function_component, html, use_effect};
+
+#[function_component(App)]
+fn app() -> Html {
+    use_effect(move || {
+        MapOptions::new("map")
+            .with_style(serde_json::json!({
+                "version": 8,
+                "sources": {
+                    "raster-tiles": {
+                        "type": "raster",
+                        "tiles": ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+                        "tileSize": 256,
+                        "minzoom": 0,
+                        "maxzoom": 19
+                    }
+                },
+                "layers": [
+                    {
+                        "id": "simple-tiles",
+                        "type": "raster",
+                        "source": "raster-tiles",
+                        "attribution": "© OpenStreetMap contributors",
+                    }
+                ],
+                "id": "blank"
+            }))
+            .with_center([0., 0.])
+            .with_zoom(0.)
+            .build()
+            .expect("Creating a map should work");
+    });
+    html! { <div id="map"></div> }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    console_error_panic_hook::set_once();
+    yew::Renderer::<App>::new().render();
+}
