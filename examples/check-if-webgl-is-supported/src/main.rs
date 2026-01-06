8c3e4fe16cfffef0838c221c5ed39b319c4ee4dc
Raul Rabadan Arroyo <raul@rabadan.dev>
2026-01-06 14:31:31 +0100

    example: Add 'Check if WebGL is supported' example

---
+++ examples/check-if-webgl-is-supported/src/main.rs
@@ -0,0 +1,62 @@
+use maplibre_gl_js::interface::MapOptions;
+use web_sys::{HtmlCanvasElement, js_sys::Reflect, wasm_bindgen::JsValue};
+use yew::{Html, function_component, html, use_effect};
+
+fn is_webgl_supported() -> bool {
+    let window = web_sys::window().expect("Window should be available");
+    let document = window.document().expect("The document should be available");
+
+    // Check if the class is directly missing
+    if let Err(_) = Reflect::get(&window, &"WebGLRenderingContext".into()) {
+        return false;
+    }
+
+    let canvas: HtmlCanvasElement = Into::<JsValue>::into(
+        document
+            .create_element("canvas")
+            .expect("Creating an element should work"),
+    )
+    .into();
+
+    // Obtain context
+    let context = match (canvas.get_context("webgl2"), canvas.get_context("webgl")) {
+        // If a JsException is raised, then it is supported but disabled
+        (Err(_), _) => return false,
+        (_, Err(_)) => return false,
+        // No context available, then it is unsupported
+        (Ok(None), Ok(None)) => return false,
+        // Context is available
+        (Ok(Some(webgl2_context)), _) => webgl2_context,
+        (Ok(None), Ok(Some(webgl_context))) => webgl_context,
+    };
+
+    // Check if the context was properly initialized
+    Reflect::get(&context, &"getParameter".into()).is_ok_and(|v| {
+        v.js_typeof()
+            .as_string()
+            .expect("Result should be a string")
+            == "function"
+    })
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let webgl_supported = is_webgl_supported();
+    use_effect(move || {
+        if webgl_supported {
+            MapOptions::new("map")
+                .with_style("https://demotiles.maplibre.org/style.json")
+                .with_center([-74.5, 40.])
+                .with_zoom(2.)
+                .build()
+                .expect("Creating a map should work");
+        }
+    });
+    html! { if webgl_supported { <div id="map"></div> } else { <p>{"WebGl not supported"}</p> } }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    console_error_panic_hook::set_once();
+    yew::Renderer::<App>::new().render();
+}
